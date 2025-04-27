b2c728640cc3b93c394093c563f7e4765490d755
DARK <hirich62@gmail.com>
2025-04-27 02:57:09 +0200

    add: french pic

--- src/components/MyImage.tsx
+++ src/components/MyImage.tsx
@@ -1,15 +1,26 @@
 import React from 'react'
 import Image from 'next/image'
+import { usePathname } from 'next/navigation';
 
 const MyImage = ({className} : {className?: string}) => {
+  const params = usePathname();
   return (
-    <div>
-       <Image src='/images/cv-img.jpg' 
+    <div>{
+      params === "/en" ? 
+      <Image src='/images/cv-img.jpg' 
+            alt="myPhoto"
+            width={693}
+            height={693}
+            className={className}
+            /> :
+            <Image src='/images/cv-img-fr.jpg' 
             alt="myPhoto"
             width={693}
             height={693}
             className={className}
             />
+      }
+       
             
     </div>
   )
